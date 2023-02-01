4eb47529ded0b7b8b7502161155f3a03f349f787
GodSamble <osgneb8359@naver.com>
2023-02-01 19:26:55 +0900

    Initial Commit

---
+++ myFirstSidePj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstSidePj
+//
+//  Created by 고영민 on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
